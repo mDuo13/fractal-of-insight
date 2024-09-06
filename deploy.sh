665d1508f849c6dd7ff97ac9eaed377e68d9dfa7
mDuo13 <mduo13@gmail.com>
2024-09-06 14:17:45 -0700

    Save deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+# Copy static files to local build
+rsync -avh --delete static/ /another/site/fractal-of-insight/static/
+
+cd /another/site/fractal-of-insight/
+
+# Deploy to hosted server
+rsync -av --delete -e ssh ./ a2:fractal-of-insight.mduo13.com/
+
