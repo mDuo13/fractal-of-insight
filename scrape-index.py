a269eb56b1673944c234befc8dea4088f2d5f251
mDuo13 <mduo13@gmail.com>
2025-03-05 10:03:50 -0800

    Update index scraper

--- scrape-index.py
+++ scrape-index.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python
 import json
-import os.path
+import os, os.path
 
 import yaml
 import requests
 
 CARDS_FOLDER = "./data/index/"
 
+print("Fetching list of sets...")
 r = requests.get("https://api.gatcg.com/option/search")
 j = r.json()
 all_sets = j["set"]
 
-all_cards = {}
+#all_cards = {}
 
 for cardset in all_sets:
     setlist = []
     prefix = cardset["value"]
-    all_cards[prefix] = []
+    #all_cards[prefix] = []
     page = 0
     total_pages = 1
 
+    print(f"Fetching {prefix}")
     while page < total_pages:
-        page += 1 
+        page += 1
+        print(f"... downloading page {page}")
         r = requests.get(f"https://api.gatcg.com/cards/search?prefix={prefix}&page={page}")
         j = r.json()
         total_pages = j["total_pages"]
         setlist += j["data"]
-        
+
+    os.makedirs(CARDS_FOLDER, exist_ok=True)        
     fname = os.path.join(CARDS_FOLDER, f"{prefix}.json")
     with open(fname, "w") as f:
         json.dump(setlist, f)
