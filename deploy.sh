dd22b2e9c265f6625fafc1dde6fa8e8f7415c20e
mDuo13 <mduo13@gmail.com>
2025-08-26 17:21:08 -0700

    Improve deploy & fix typo

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ rsync -avh --delete static/ /another/site/fractal-of-insight/static/
 
 cd /another/site/fractal-of-insight/
 
-# Deploy to hosted server
+# Deploy to hosted server. Static first so people don't cache the old one
+rsync -av --delete --exclude .htaccess --checksum -e ssh ./static/ a2:fractal-of-insight.mduo13.com/static/
 rsync -av --delete --exclude .htaccess --checksum -e ssh ./ a2:fractal-of-insight.mduo13.com/
 
